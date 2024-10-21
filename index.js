67e12d7ee01c15041144ed7cb394fa8d443538ee
RacconThibault <tyshia.pro@gmail.com>
2024-10-21 18:18:45 +0200

    install Mongo

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const config = require("./config");
 
-mongoose.connect(
-  `mongodb+srv://tyshiapro:<db_password>@cluster0.f28s5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-);
+mongoose
+  .connect(
+    `mongodb+srv://${config.mongoUser}:${config.mongoSecret}@cluster0.f28s5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(
+    () => {
+      console.log("Connected to the database!");
+    },
+    (err) => {
+      console.error("Connection error", err);
+    }
+  );
 
 app.use(cors());
 app.use(bodyParser.json());
